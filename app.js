d9e6fe1a95cafd5636c0d9f3785aa05c51c4320a
Victor Santos <santos.victors2000@gmail.com>
2023-08-22 12:42:52 -0300

    Commit

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express()
 const path = require("path")
 const PORT = 3232
 
-app.get("/", ()=>{
-    app.use(express.static(path.join(__dirname, 'app')))
-})
+app.use("/", express.static(path.join(__dirname, 'app')))
 
 app.listen(PORT)
